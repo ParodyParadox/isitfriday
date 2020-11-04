a1b1c5221b844f49318a43832468589d04c502cd
ParodyParadox <isaachuffman1122@gmail.com>
2020-11-04 11:41:36 -0500

    Added some starter code

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+console.log("Hello Royals!");
 
+const today = new Date();
+
+function checkForFriday(currentDate) {
+  if(currentDate.getDay() === 5) {
+    document.querySelector("#answer").innerHTML = 'YES!';
+  } else {
+    document.querySelector("#answer").innerHTML = 'NO :(';
+  }
+}
+
+checkForFriday(tody);
